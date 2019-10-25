0ddf37798545f009f9dfc80199e2e1497332853d
Jin Kwon <onacit@gmail.com>
2019-10-25 21:32:29 +0900

    updated

--- src/main/java/com/github/jinahya/bit/io/BitReader.java
+++
@@ -1,8 +0,0 @@
-package com.github.jinahya.bit.io;
-
-import java.io.IOException;
-
-interface BitReader<T> {
-
-    void read(T value, BitInput input) throws IOException;
-}
--- src/main/java/com/github/jinahya/bit/io/BitWriter.java
+++
@@ -1,8 +0,0 @@
-package com.github.jinahya.bit.io;
-
-import java.io.IOException;
-
-interface BitWriter<T> {
-
-    void write(T value, BitOutput output) throws IOException;
-}

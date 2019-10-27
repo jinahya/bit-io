d863d3e4c973f121201bacbc8ad203c3132cf33c
Jin Kwon <onacit@gmail.com>
2019-10-27 16:57:04 +0900

    updated

--- src/main/java/com/github/jinahya/bit/io/BitWriters.java
+++ src/main/java/com/github/jinahya/bit/io/BitWriters.java
@@ -21,23 +21,37 @@ package com.github.jinahya.bit.io;
  */
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 final class BitWriters {
 
     // -----------------------------------------------------------------------------------------------------------------
-    public static final BitWriter<BitWritable> INSTANCE_FOR_BIT_WRITABLES = new BitWriter<BitWritable>() {
-        @Override
-        public void write(final BitWritable value, final BitOutput output) throws IOException {
-            value.write(output);
+    private static Map<Class<?>, BitWriter<?>> BIT_WRITERS;
+
+    private static Map<Class<?>, BitWriter<?>> bitWriters() {
+        if (BIT_WRITERS == null) {
+            BIT_WRITERS = new WeakHashMap<>();
         }
-    };
+        return BIT_WRITERS;
+    }
+
+    private static <T extends BitWritable> BitWriter<T> bitWRiter(final Class<? extends T> type) {
+        @SuppressWarnings({"unchecked"})
+        BitWriter<T> value = (BitWriter<T>) bitWriters().get(type);
+        if (value == null) {
+            value = new BitWriter<T>() {
+                @Override
+                public void write(final T value, final BitOutput output) throws IOException {
+                    value.write(output);
+                }
+            };
+            BIT_WRITERS.put(type, value);
+        }
+        return value;
+    }
 
     public static <T extends BitWritable> BitWriter<T> bitWriterFor(final Class<? extends T> type) {
-        return new BitWriter<T>() {
-            @Override
-            public void write(final T value, final BitOutput output) throws IOException {
-                value.write(output);
-            }
-        };
+        return bitWRiter(type);
     }
 }
